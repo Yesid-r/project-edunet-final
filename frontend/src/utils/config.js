12acb185c3e47b3defa7be679ac9898f14e21c61
Yesid Rincon <dairo.rincon@uptc.edu.co>
2023-05-01 15:47:50 -0500

    add url backend  project

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'https://edunetapi.onrender.com'
-export const BASE_URL = 'http://localhost:4000'
+export const BASE_URL = 'https://edunetfinal.onrender.com'
+// export const BASE_URL = 'http://localhost:4000'
 export const ADMIN_EMAIL = 'admin@gmail.com'
